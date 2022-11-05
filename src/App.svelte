8a21b47051c1e621375756104caf47e99f3a0a03
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-11-04 22:02:55 -0700

    added NFTs pictures

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,10 @@
 </script>
 
 <main>
+  <div>
+    <img src="./images/Game of Sheep.png" width = "300" alt="GameofSheep" />
+  </div>
+  <br><br>
   <Box>
     <div>
       <div class="note">Roll Dice</div>
@@ -103,6 +107,30 @@
       rel="noopener noreferrer nofollow">Taquito</a
     >!
   </div>
+ 
+<div>
+  <h1>NFTs</h1>
+  <h2>Benefits</h2>
+  <img src="./images/Benefits/Fire Fighting Equipment.png" width = "350" alt="Fire Fighting Equipment" />
+  <img src="./images/Benefits/Hay Stack.png" width = "350" alt="Hay Stack" />
+  <img src="./images/Benefits/Insurance Policy for Stud Ram.png" width = "350" alt="Insurance Policy for Stud Ram" />
+  <h2>Control Programmes</h2>
+  <img src="./images/Control Programmes/Control of Weeds and Insects.png" width = "350" alt="Control of Weeds and Insects" />
+  <img src="./images/Control Programmes/Fertilized Pasture.png" width = "350" alt="Fertilized Pasture" />
+  <img src="./images/Control Programmes/Worm Control Programme.png" width = "350" alt="Worm Control Programme" />
+  <h2>Liabilities</h2>
+  <img src="./images/Liabilities/High Stock Prices Ruling.png" width = "350" alt="High Stock Prices Ruling" />
+  <img src="./images/Liabilities/New Shearing Shed.png" width = "350" alt="New Shearing Shed" />
+  <img src="./images/Liabilities/Blow Fly Wave.png" width = "350" alt="Blow Fly Wave" />
+  <h2>Stud Rams</h2> 
+  <img src="./images/Stud Rams/Elmsford Park Rex.png" width = "350" alt="Elmsford Park Rex" />
+  <img src="./images/Stud Rams/King of Warramboo.png" width = "350" alt="King of Warramboo" />
+  <img src="./images/Stud Rams/Lachlan Lad.png" width = "350" alt="Lachlan Lad" />
+  <img src="./images/Stud Rams/Mitchell's Pride.png" width = "350" alt="Mitchell's Pride" />
+  <img src="./images/Stud Rams/Winton Boy II.png" width = "350" alt="Winton Boy II" />
+</div>
+
+
 </main>
 
 <style>
--- src/lib/RollDice.svelte
+++ src/lib/RollDice.svelte
@@ -1,7 +1,32 @@
 <script>
     let count = 0;
-    const randomDice1 = Math.floor(6 * Math.random()) + 1;
-    const randomDice2 = Math.floor(6 * Math.random()) + 1;
+
+    // https://github.com/bryc/code/blob/master/jshash/PRNGs.md
+// xoshiro128+ (128-bit state generator in 32-bit)
+function xoshiro128p() {
+  // Using the same value for each seed is _screamingly_ wrong
+  // but this is 'good enough' for a toy function.
+  let a = Date.now(),
+    b = Date.now(),
+    c = Date.now(),
+    d = Date.now()
+  return function () {
+    let t = b << 9,
+      r = a + d
+    c = c ^ a
+    d = d ^ b
+    b = b ^ c
+    a = a ^ d
+    c = c ^ t
+    d = (d << 11) | (d >>> 21)
+    return (r >>> 0) / 4294967296
+  }
+}
+
+    // @ts-ignore
+    const randomDice1 = Math.floor(6 * xoshiro128p()) + 1;
+    // @ts-ignore
+    const randomDice2 = Math.floor(6 * xoshiro128p()) + 1;
     const diceRoll = randomDice1 + randomDice2;
 
     const increment = () => {
